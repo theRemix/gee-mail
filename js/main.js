ca4b8a2364d6a9fefd9f1a43657bf9f136bf70de
Jon Borgonia <jon@gomagames.com>
2014-01-18 21:28:04 -1000

    objective 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
+var messages_inbox;
+
 (function(){
-  console.log("hello world");
+  window.onload = function() {
+    messages_inbox = document.getElementById("messages_inbox");
+    populateInbox(); 
+  }
 })();
+
+
+function populateInbox () {
+  if(window.geemails.length > 0){
+    // remove any empty_box messages since it's no longer empty
+    messages_inbox.innerHTML = "";
+
+    for(i in window.geemails){
+      appendMessage(messages_inbox, window.geemails[i]);
+    }
+  }
+}
+
+function appendMessage (box, message) {
+  var item = document.createElement("li");
+  item.appendChild(spanElement("box_list_from", message.sender));
+  item.appendChild(spanElement("box_list_subject", message.subject));
+  item.appendChild(spanElement("box_list_date", message.date));
+  item.setAttribute("data-body", message.body);
+  item.onclick = click_message;
+  box.appendChild(item);
+}
+
+function spanElement (class_name, inner) {
+  var element = document.createElement('span');
+  element.className = class_name;
+  element.innerHTML = inner;
+  return element;
+}
+
+function click_message () {
+  
+}
